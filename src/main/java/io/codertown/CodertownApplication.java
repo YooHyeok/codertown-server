47a1d586abcd7bbc2bf1788a36765aa6b2f58e84
webdevyoo <yjk7454@naver.com>
2023-07-17 10:58:46 +0900

    init commit

---
+++ src/main/java/io/codertown/CodertownApplication.java
@@ -0,0 +1,13 @@
+package io.codertown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodertownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodertownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/codertown/CodertownApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codertown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodertownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
