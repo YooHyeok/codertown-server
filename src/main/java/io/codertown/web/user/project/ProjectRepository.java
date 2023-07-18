a9316db90a285f99a2111c1f4dafd31cc4c159e2
webdevyoo <yjk7454@naver.com>
2023-07-18 18:39:32 +0900

    ProjectRepository, ProjectService 클래스 추가

---
+++ src/main/java/io/codertown/web/user/project/ProjectRepository.java
@@ -0,0 +1,6 @@
+package io.codertown.web.user.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
---
+++ src/main/java/io/codertown/web/user/project/ProjectService.java
@@ -0,0 +1,7 @@
+package io.codertown.web.user.project;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+}
