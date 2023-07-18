59fa2e67089754afb0cc5984c1e2f610405c750c
webdevyoo <yjk7454@naver.com>
2023-07-18 15:17:17 +0900

    UserService, UserRepository 추가

---
+++ src/main/java/io/codertown/web/user/UserService.java
@@ -0,0 +1,17 @@
+package io.codertown.web.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * *****************************************************<p>
+ * 패키지:io.codertown.user<p>
+ * 파일 : UserService.java<p>
+ * 프로그램 설명 : 회원 서비스 클래스<p>
+ * 연관테이블 : user<p>
+ * 담당 : 유재혁<p>
+ * *****************************************************<p>
+ */
+@Service
+public class UserService {
+}
