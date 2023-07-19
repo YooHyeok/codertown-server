542f4f94e5589d4479208093be250d48176f97bb
webdevyoo <yjk7454@naver.com>
2023-07-19 12:01:40 +0900

    Coggle Entity 클래스 추가

---
+++ src/main/java/io/codertown/web/coggle/Coggle.java
@@ -0,0 +1,28 @@
+package io.codertown.web.coggle;
+
+import io.codertown.support.base.BaseTimeStampEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Check;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Coggle extends BaseTimeStampEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long coggleNo;
+    @Check(constraints = "IN ('T', 'C', 'D')")
+    private Character category;
+    private String title;
+    private String content;
+
+    @ColumnDefault("false")
+    private Boolean status; // 글상태 True : 삭제 | False : 정상
+
+}
