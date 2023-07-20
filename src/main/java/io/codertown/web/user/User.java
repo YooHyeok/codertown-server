afd3a6931c883ae7f6601633973e7e7a095ff13c
webdevyoo <yjk7454@naver.com>
2023-07-20 15:02:51 +0900

    Security UserDetails 인터페이스 구현 - User Entity

--- src/main/java/io/codertown/web/user/User.java
+++ src/main/java/io/codertown/web/user/User.java
@@ -7,9 +7,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -24,7 +27,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class User extends BaseTimeStampEntity {
+public class User extends BaseTimeStampEntity implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_NO")
     private Long id;
@@ -44,4 +47,39 @@ public class User extends BaseTimeStampEntity {
     @OneToMany(mappedBy = "recruitUser")
     private List<Recruit> recruitUsers = new ArrayList<>();
 
+    /* === Security UserDetails 구현 === */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
