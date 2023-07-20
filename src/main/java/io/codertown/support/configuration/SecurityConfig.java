2f4f126ba9350e49b322d72036c42d6942ffc69d
webdevyoo <yjk7454@naver.com>
2023-07-20 16:06:40 +0900

    SecurityConfig 클래스 추가 및 Security 설정

---
+++ src/main/java/io/codertown/support/configuration/SecurityConfig.java
@@ -0,0 +1,36 @@
+package io.codertown.support.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+        http.csrf().disable(); // csrf기능 OFF
+        http.httpBasic().disable() // HttpBasicAuth 기반 로그인 기능 OFF
+                .authorizeHttpRequests() // 인가 정책 시작
+                .antMatchers("/**/*").permitAll() // 모든 경로 접근 허가
+                .anyRequest().authenticated(); //어떠한 요청에도 인증 허용
+//		.antMatchers("/login**", "/web-respirces/**", "/actuator/**").permitAll()
+//		.antMatchers("/admit/**").hasRole("ADMIN")
+//		.antMatchers("/user/**").hasRole("USER")
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //세션 제어 (JWT토큰 대체)
+        return http.build();
+    }
+}
