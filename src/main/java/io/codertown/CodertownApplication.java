8c7bc1979577059d3a81395d64f97fb87d8b7265
webdevyoo <yjk7454@naver.com>
2023-07-21 11:30:16 +0900

    ADD: AUDITING null - Application클래스에 @EnableJpaAuditng 어노테이션 추가

--- src/main/java/io/codertown/CodertownApplication.java
+++ src/main/java/io/codertown/CodertownApplication.java
@@ -2,7 +2,9 @@ package io.codertown;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing(modifyOnCreate = false) // 저장 시점에 created데이터만 입력 (lastModified는 null로 처리)
 @SpringBootApplication
 public class CodertownApplication {
 
