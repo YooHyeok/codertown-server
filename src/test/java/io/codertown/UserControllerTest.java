b612dab0e2ba17ef376d34b9b98de541ecb9fed2
webdevyoo <yjk7454@naver.com>
2023-07-24 22:03:14 +0900

    MOD: UserControllerTest 결과값 수정

--- src/test/java/io/codertown/UserControllerTest.java
+++ src/test/java/io/codertown/UserControllerTest.java
@@ -3,7 +3,9 @@ package io.codertown;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.codertown.support.configuration.SecurityConfig;
 import io.codertown.support.configuration.WebConfig;
+import io.codertown.support.jwt.JwtTokenProvider;
 import io.codertown.web.user.SignUpRequest;
+import io.codertown.web.user.SignUpResult;
 import io.codertown.web.user.UserController;
 import io.codertown.web.user.UserService;
 import org.junit.jupiter.api.DisplayName;
@@ -31,14 +33,17 @@ public class UserControllerTest {
     @MockBean //실제 빈 객체가 아닌 Mock(가짜) 객체를 생성해서 주입하는 역할을 수행한다.
     UserService userService; // 실제 행위를 수행하지 않는다. - Mockito의 given() 메서드를 통해 동작을 정의한다.
 
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
+
     @Test
     @DisplayName("USER 회원가입 데이터 생성 테스트")
     void createUserTest() throws Exception {
         BDDMockito.given(
                 userService.signUp(
-                        new SignUpRequest("webdevyoo@gmail.com", "1234", null, "1234", null)
+                        new SignUpRequest("webdevyoo@gmail.com", "1234", null, null, null, "USER_ROLE")
                 )
-        ).willReturn(Boolean.TRUE);
+        ).willReturn(new SignUpResult(true, 0, "Success"));
 
         SignUpRequest buildDto = SignUpRequest.builder()
                 .email("webdevyoo@gmail.com")
@@ -46,6 +51,7 @@ public class UserControllerTest {
                 .profileIcon(null)
                 .password("1234")
                 .gender(null)
+                .role("USER_ROLE")
                 .build();
 
         ObjectMapper mapper = new ObjectMapper();
@@ -53,15 +59,22 @@ public class UserControllerTest {
 
         mockMvc.perform(
                 MockMvcRequestBuilders
-                        .post("/signUp")
+                        .post("/sign-up")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON)
                 ).andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("false"))
+                /* then 결과값 검증(exists 존재여부) - 46Line의 willReturn에 결과값 가정 */
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").exists())
+                /* then 결과값 검증(value 일치여부) - 46Line의 willReturn에 결과값 가정 */
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(0))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("Success"))
                 .andDo(MockMvcResultHandlers.print());
 
         // 모의객체인 userService로부터 signUp 메소드가 1회 호출되었는지 확인.
-        Mockito.verify(userService, Mockito.times(1)).signUp(new SignUpRequest("webdevyoo@gmail.com", "1234", null, null, null));
+        Mockito.verify(userService, Mockito.times(1)).signUp(new SignUpRequest("webdevyoo@gmail.com", "1234", null, null, null, "USER_ROLE"));
 //        Mockito.verify(userService).signUp(new CreateUserRequestDto("webdevyoo","gmail.com", null, "1234", "유혁스쿨", null, null));
     }
 }
