966e89975a02816ebbc20d295b93b88852c83a1c
webdevyoo <yjk7454@naver.com>
2023-08-02 10:37:46 +0900

    ADD: 코글 저장 API,기능 추가 및 구현

--- src/main/java/io/codertown/web/controller/CoggleController.java
+++ src/main/java/io/codertown/web/controller/CoggleController.java
@@ -1,16 +1,32 @@
 package io.codertown.web.controller;
 
 import io.codertown.web.payload.CoggleSaveRequest;
+import io.codertown.web.service.CoogleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CoggleController {
 
-    @PostMapping("/coggle-save")
-    public void coggleSave(@RequestBody CoggleSaveRequest request) {
-        System.out.println("request = " + request);
+    private final CoogleService coggleService;
 
+    /**
+     * 코글 저장 API
+     * @param request
+     * @return 성공: TRUE | 실패: FALSE
+     */
+    @PostMapping("/coggle-save")
+    public ResponseEntity<Boolean> coggleSave(@RequestBody CoggleSaveRequest request) {
+        try {
+            Boolean result = coggleService.coggleSave(request);
+            return ResponseEntity.ok(result);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- src/main/java/io/codertown/web/service/CoogleService.java
+++ src/main/java/io/codertown/web/service/CoogleService.java
@@ -1,7 +1,39 @@
 package io.codertown.web.service;
 
+import io.codertown.web.entity.Coggle;
+import io.codertown.web.entity.user.User;
+import io.codertown.web.payload.CoggleSaveRequest;
+import io.codertown.web.repository.CoogleRepository;
+import io.codertown.web.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CoogleService {
+
+    private final CoogleRepository coogleRepository;
+    private final UserRepository userRepository;
+
+    /**
+     * 코글 저장
+     * @param request
+     * @return 성공: TRUE | 실패: FALSE
+     */
+    public Boolean coggleSave(CoggleSaveRequest request) {
+        try {
+            User writer = (User)userRepository.findByEmail(request.getWriter());
+            Coggle coggle = Coggle.builder()
+                    .category(request.getCategory())
+                    .title(request.getTitle())
+                    .content(request.getContent())
+                    .status(false)
+                    .user(writer)
+                    .build();
+            coogleRepository.save(coggle);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
