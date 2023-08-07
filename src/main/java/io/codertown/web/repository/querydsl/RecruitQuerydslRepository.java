112c1917c805b35054d5be7f4ebea092e6dcbaad
webdevyoo <yjk7454@naver.com>
2023-08-07 15:46:02 +0900

    CREATE&ADD: QueryDsl용 Repository인터페이스 추가

---
+++ src/main/java/io/codertown/web/repository/querydsl/RecruitQuerydslRepository.java
@@ -0,0 +1,9 @@
+package io.codertown.web.repository.querydsl;
+
+import io.codertown.web.entity.recruit.Recruit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RecruitQuerydslRepository {
+    Page<Recruit> findByType(String dType, Pageable pageable);
+}
