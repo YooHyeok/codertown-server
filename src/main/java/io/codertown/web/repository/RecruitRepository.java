1af931122f9094bd1f40b4355d78a2b862c54055
webdevyoo <yjk7454@naver.com>
2023-08-07 16:42:26 +0900

    ADD: RecruitQuerydslRepository 및 QuerydslPredicateExecutor RecruitRepository에서 상속처리

--- src/main/java/io/codertown/web/repository/RecruitRepository.java
+++ src/main/java/io/codertown/web/repository/RecruitRepository.java
@@ -1,12 +1,14 @@
 package io.codertown.web.repository;
 
 import io.codertown.web.entity.recruit.Recruit;
+import io.codertown.web.repository.querydsl.RecruitQuerydslRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface RecruitRepository extends JpaRepository<Recruit, Long> {
+public interface RecruitRepository extends JpaRepository<Recruit, Long>, RecruitQuerydslRepository, QuerydslPredicateExecutor<Recruit> {
     @Query("select r from Recruit r where TYPE(r) = Cokkiri")
     Page<Recruit> findByCategory(PageRequest pageRequest);
 }
