c4935c23de295d46fe312cfa68754ae96cefa16a
webdevyoo <yjk7454@naver.com>
2023-08-08 10:25:33 +0900

    ADD: MammothDto 클래스에 Entity Dto 변환 Builder메소드 추가

--- src/main/java/io/codertown/web/dto/MammothDto.java
+++ src/main/java/io/codertown/web/dto/MammothDto.java
@@ -1,5 +1,6 @@
 package io.codertown.web.dto;
 
+import io.codertown.web.entity.recruit.Mammoth;
 import lombok.Builder;
 import lombok.Data;
 
@@ -13,4 +14,15 @@ public class MammothDto {
     private UserDto writer; //추후 writer정보 변경가능
 
     private String location;
+
+    public static MammothDto entityToDto(Mammoth mammoth, UserDto userDto) {
+        return MammothDto.builder()
+                .title(mammoth.getTitle()) // 코끼리 글 제목
+                .link(mammoth.getLink())
+                .content(mammoth.getContent()) // 코끼리 글 내용
+                .writer(userDto) // 코끼리 글 작성자 (추후 String값으로 수정)
+//                .writer(cokkiri.getRecruitUser().getEmail()) // 코끼리 글 작성자 (추후 닉네임으로 수정 가능)
+                .location(mammoth.getLocation()) // 목표 기간(주)
+                .build();
+    }
 }
