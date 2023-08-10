d3a5ef9ad91faca86d1b0f87f9709b7788f880a0
webdevyoo <yjk7454@naver.com>
2023-08-10 14:47:17 +0900

    MOD: Coggle 목록 조회 JPQL User FetchJoin 적용

--- src/main/java/io/codertown/web/repository/CoggleRepository.java
+++ src/main/java/io/codertown/web/repository/CoggleRepository.java
@@ -4,6 +4,7 @@ import io.codertown.web.entity.Coggle;
 import io.codertown.web.entity.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -20,6 +21,8 @@ public interface CoggleRepository extends JpaRepository<Coggle, Long> {
      * @param pageRequest
      * @return
      */
+
+    @EntityGraph(attributePaths = "user")
     @Query("SELECT c FROM Coggle c WHERE (:category IS NULL OR c.category = :category) AND (:user IS NULL OR c.user = :user) AND (:title IS NULL OR c.title = :title)")
     Page<Coggle> findByCategoryAndUser(@Param("category") Character category, @Param("user") User user, @Param("title") String title, PageRequest pageRequest);
 }
