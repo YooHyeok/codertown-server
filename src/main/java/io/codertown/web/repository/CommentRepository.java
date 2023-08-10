c764e5c39a5f1170ba05abf11ac42c76a8f9dc5d
webdevyoo <yjk7454@naver.com>
2023-08-10 15:49:12 +0900

    MOD: 대댓글 조회시 Collection List데이터 @EntityGraph FetchJoin 처리

--- src/main/java/io/codertown/web/repository/CommentRepository.java
+++ src/main/java/io/codertown/web/repository/CommentRepository.java
@@ -2,6 +2,7 @@ package io.codertown.web.repository;
 
 import io.codertown.web.entity.Coggle;
 import io.codertown.web.entity.Comment;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +10,8 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    @EntityGraph(attributePaths = {"user", "children"})
     @Query(value = "select c from Comment c where c.coggle = :coggle order by c.parent.id asc nulls first , c.firstRegDate asc")
     List<Comment> findByCoggle(@Param("coggle") Coggle coggle);
 }
