3dfd055e9a3eaa013b53e744bdabeb20e779d1d9
webdevyoo <yjk7454@naver.com>
2023-08-22 11:31:41 +0900

    MOD: ProjectPartSaveDto 디테일 페이지용 지원 현황 필드 추가

--- src/main/java/io/codertown/web/dto/ProjectPartSaveDto.java
+++ src/main/java/io/codertown/web/dto/ProjectPartSaveDto.java
@@ -15,12 +15,14 @@ public class ProjectPartSaveDto {
     private Long partNo;
     private String partName;
     private int recruitCount;
+    private int currentCount;
 
     public static ProjectPartSaveDto entityToDto(ProjectPart projectPart) {
         return ProjectPartSaveDto.builder()
                 .partNo(projectPart.getPart().getId()) // 파트 번호
                 .partName(projectPart.getPart().getPartName()) // 파트 이름
                 .recruitCount(projectPart.getRecruitCount()) // 모집 인원
+                .currentCount(projectPart.getCurrentCount()) // 지원 인원
                 .build();
     }
 }
