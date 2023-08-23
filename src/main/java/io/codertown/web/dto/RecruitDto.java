ab3fece1d10c41205b8fb485a0a6a7d7f695d0cb
webdevyoo <yjk7454@naver.com>
2023-08-23 16:13:33 +0900

    MOD: RecruitDto 글번호, 최종등록일자 필드 추가

--- src/main/java/io/codertown/web/dto/RecruitDto.java
+++ src/main/java/io/codertown/web/dto/RecruitDto.java
@@ -5,9 +5,12 @@ import io.codertown.web.entity.recruit.Mammoth;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class RecruitDto {
+    private Long recruitNo;
     private String category;
     private String title;
     private String content;
@@ -16,13 +19,17 @@ public class RecruitDto {
     private UserDto writer; //추후 writer정보 변경가능
     private String location;
     private Integer objectWeek;
+    private LocalDateTime firstRegDate;
 
     public static RecruitDto cokkiriEntityToDto(Cokkiri cokkiri, UserDto userDto, String dType) {
+        System.out.println(cokkiri.getFirstRegDate());
         return RecruitDto.builder()
+                .recruitNo(cokkiri.getId())
                 .category(dType)
                 .title(cokkiri.getTitle()) // 코끼리 글 제목
                 .link(cokkiri.getLink())
                 .content(cokkiri.getContent()) // 코끼리 글 내용
+                .firstRegDate(cokkiri.getFirstRegDate())
                 .writer(userDto) // 코끼리 글 작성자 (추후 String값으로 수정)
                 .objectWeek(cokkiri.getObjectWeek()) // 목표 기간(주)
                 .build();
