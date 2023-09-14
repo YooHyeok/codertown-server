98e4ac35d465acfd27db22d238dd4158b553468f
webdevyoo <yjk7454@naver.com>
2023-09-14 09:09:05 +0900

    MOD: 프로젝트 테스트용 리포지토리 클래스 추가 및 테스트 패키지 이동

--- src/test/java/io/codertown/project_test/ProjectRepositoryTest.java
+++ src/test/java/io/codertown/project_test/ProjectRepositoryTest.java
@@ -1,7 +1,6 @@
-package io.codertown;
+package io.codertown.project_test;
 
 import io.codertown.web.entity.user.User;
-import io.codertown.web.repository.ProjectRepository;
 import io.codertown.web.repository.UserProjectRepository;
 import io.codertown.web.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -23,14 +22,14 @@ public class ProjectRepositoryTest {
     private UserProjectRepository userProjectRepository;
 
     @Autowired
-    private ProjectRepository projectRepository;
+    private ProjectTestRepository projectTestRepository;
 
     @Test
     @DisplayName("내가 참여중인 프로젝트 조회(나의 파트 함께 조회) 테스트 1")
     void findJoinedProjectTest1() {
 /*
         long startTime = System.currentTimeMillis();
-        projectRepository.findJoinedProjectTest1().forEach(project -> {
+        projectTestRepository.findJoinedProjectTest1().forEach(project -> {
             System.out.println("project = " + project.getProject());
             System.out.println("project = " + project.getUserProject());
         });
@@ -44,11 +43,11 @@ public class ProjectRepositoryTest {
     @DisplayName("내가 참여중인 프로젝트 조회(나의 파트 함께 조회) 테스트 2")
     void findJoinedProjectTest2() {
         long startTime = System.currentTimeMillis();
-        projectRepository.findJoinedProjectTest2().forEach(project -> {
+        projectTestRepository.findJoinedProjectTest2().forEach(project -> {
             System.out.println("project = " + project.getProject());
             System.out.println("project = " + project.getProjectPart().getPart());
         });
-        /*projectRepository.findJoinedProjectTest2().stream().map(object -> {
+        /*projectTestRepository.findJoinedProjectTest2().stream().map(object -> {
             System.out.println("object.length = " + object.length);
             return new JoinedProjectTestDto((Project) object[0], (UserProject) object[1], (ProjectPart) object[2]);
         }).collect(Collectors.toList()).forEach(project -> {
@@ -56,7 +55,7 @@ public class ProjectRepositoryTest {
             System.out.println("project = " + project.getUserProject());
             System.out.println("project = " + project.getProjectPart());
         });*/
-        /*projectRepository.findJoinedProjectTest2().forEach(o -> {
+        /*projectTestRepository.findJoinedProjectTest2().forEach(o -> {
             System.out.println("o[1] = " + o[0]);
             System.out.println("o[2] = " + o[1]);
         });*/
@@ -69,14 +68,14 @@ public class ProjectRepositoryTest {
     @DisplayName("내가 참여중인 프로젝트 조회(나의 파트 함께 조회) 테스트 3")
     void findJoinedProjectTest3() {
         /*long startTime = System.currentTimeMillis();
-        projectRepository.findJoinedProjectTest3().stream().map(tuple -> {
+        projectTestRepository.findJoinedProjectTest3().stream().map(tuple -> {
             return new JoinedProjectTestDto(tuple.get("pt", Project.class), tuple.get("up", UserProject.class), tuple.get("pp", ProjectPart.class));
         }).collect(Collectors.toList()).forEach(project -> {
             System.out.println("project = " + project.getProject());
             System.out.println("project = " + project.getUserProject());
             System.out.println("project = " + project.getProjectPart());
         });
-        projectRepository.findJoinedProjectTest3().forEach(o -> {
+        projectTestRepository.findJoinedProjectTest3().forEach(o -> {
             System.out.println("o[0] = " + o.get("pt", Project.class));
             System.out.println("o[1] = " + o.get("up", UserProject.class));
         });
@@ -89,7 +88,7 @@ public class ProjectRepositoryTest {
     @DisplayName("내가 참여중인 프로젝트 조회(나의 파트 함께 조회) 테스트 4")
     void findJoinedProjectTest4() {
         User loginUser = (User) userRepository.findByEmail("webdevyoo@gmail.com");
-        /*List<Map<String, Object>> joinedProject = projectRepository.findJoinedProject(loginUser);
+        /*List<Map<String, Object>> joinedProject = projectTestRepository.findJoinedProject(loginUser);
 
         joinedProject.forEach(stringObjectMap -> {
             System.out.println(stringObjectMap.get("part"));
---
+++ src/test/java/io/codertown/project_test/ProjectTestRepository.java
@@ -0,0 +1,56 @@
+package io.codertown.project_test;
+
+import io.codertown.web.dto.JoinedProjectSimpleConvertDto;
+import io.codertown.web.dto.JoinedProjectTestDto;
+import io.codertown.web.entity.project.Project;
+import io.codertown.web.entity.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProjectTestRepository extends JpaRepository<Project, Long> {
+
+//    @Query("SELECT NEW io.codertown.web.dto.JoinedProjectDto(p, up) FROM Project p LEFT JOIN fetch UserProject up ON (up.id = p.id) LEFT JOIN fetch ProjectPart pp ON (pp.id = up.id) where up.projectUser.email = 'webdevyoo@gmail.com'")
+//    List<JoinedProjectDto> findJoinedProjectTest1();
+
+    @Query("SELECT NEW io.codertown.web.dto.JoinedProjectTestDto(p, up, pp) FROM Project p " +
+            "LEFT JOIN fetch UserProject up ON (up.project.id = p.id) " +
+            "LEFT JOIN fetch ProjectPart pp ON (pp.id = up.projectPart.id) " +
+            "where up.projectUser.email = 'webdevyoo@gmail.com'")
+    List<JoinedProjectTestDto> findJoinedProjectTest2();
+
+    /*@Query("SELECT p, up, pp FROM Project p " +
+            "LEFT JOIN fetch UserProject up ON (up.project.id = p.id) " +
+            "LEFT JOIN fetch ProjectPart pp ON (pp.id = up.projectPart.id) " +
+            "where up.projectUser.email = 'webdevyoo@gmail.com'")
+    List<Object[]> findJoinedProjectTest2();*/
+
+/*
+    @Query("SELECT pt AS pt, up AS up, pp AS pp FROM Project pt " +
+            "LEFT JOIN fetch UserProject as up ON (up.id = pt.id) " +
+            "LEFT JOIN fetch ProjectPart as pp ON (pp.id = up.id) " +
+            "where up.projectUser.email = 'webdevyoo@gmail.com'")
+    List<Tuple> findJoinedProjectTest3();
+*/
+
+
+    /* 객체 탐색으로 JOIN하면 DTO변환 혹은 Tuple, Object[] 조회가 불가능해진다. */
+    /*@Query("SELECT p as project, pp.part.id as participationPartNo, pp.part as participationPartName FROM Project p " +
+            "LEFT JOIN FETCH p.projects up " +
+            "LEFT JOIN FETCH up.projectPart pp " +
+            "WHERE up.projectUser = :loginUser")
+    List<Map<String, Object>> findJoinedProject(@Param("loginUser") User loginUser);*/
+
+
+    @Query("SELECT NEW io.codertown.web.dto.JoinedProjectSimpleConvertDto(p, pp) FROM Project p " +
+            "LEFT JOIN fetch UserProject up ON (up.project.id = p.id) " +
+            "LEFT JOIN fetch ProjectPart pp ON (pp.id = up.projectPart.id) " +
+            "where up.projectUser = :loginUser")
+    Page<JoinedProjectSimpleConvertDto> findJoinedProject(@Param("loginUser") User loginUser, PageRequest pageRequest);
+
+
+}
