c7b309f98d495150de1f82378360bd35217c0855
webdevyoo <yjk7454@naver.com>
2023-10-24 15:14:18 +0900

    MOD: 멘션 대댓글 정렬 문제 JPQL 코드 수정

--- src/main/java/io/codertown/web/repository/CommentRepository.java
+++ src/main/java/io/codertown/web/repository/CommentRepository.java
@@ -12,6 +12,7 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @EntityGraph(attributePaths = {"writer", "children"})
-    @Query(value = "select c from Comment c where c.coggle = :coggle order by c.parent.id asc nulls first ,  c.firstRegDate asc, c.id asc")
+    @Query(value = "select c from Comment c where c.coggle = :coggle AND c.parent.id is null order by c.firstRegDate, c.id asc")
+//    @Query(value = "select c from Comment c where c.coggle = :coggle order by c.parent.id asc nulls first ,  c.firstRegDate asc, c.id asc")
     List<Comment> findByCoggle(@Param("coggle") Coggle coggle);
 }
